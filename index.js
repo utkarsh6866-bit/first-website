16db518a945f3c2cd580565b5bfec10851ad7eb1
Utkarsh <ukarsh6866@gmail.com>
2025-12-28 04:47:40 +0530

    Initial basic calculator website

---
+++ index.js
@@ -0,0 +1,17 @@
+  const display = document.getElementById("display");
+
+    function press(value) {
+        display.value += value;
+    }
+
+    function clearDisplay() {
+        display.value = "";
+    }
+
+    function calculate() {
+        try {
+            display.value = eval(display.value);
+        } catch {
+            display.value = "❌ Error";
+        }
+    }
